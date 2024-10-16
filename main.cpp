3c61cb99a2061093e661865edfdff8e6d2821463
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-10-16 14:58:45 -0500

    Implement console

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
 #include "board.hpp"
+#include "console.hpp"
 #include "game_state.hpp"
 
 int main()
 {
   Board board;
   GameState game_state(&board);
+  Console console(&board);
   char mark = 'X';
   int move;
 
   while (game_state.current_state() == "in-progress")
   {
+    std::cout << console.display();
     std::cout << "What is your move? ";
     std::cin >> move;
     board.move(move, mark);
-    // print the board to the screen
     // get some input from the user (with validation)
     // make a move on the board
     // switch the marker from x to o or vice versa
